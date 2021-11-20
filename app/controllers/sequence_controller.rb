2ca06780f25c37db3ca1f2002b940106a5f0a836
MrAlexol <Alex02072002@yandex.ru>
2021-11-21 00:36:12 +0300

    v. 3. AJAX

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -5,19 +5,29 @@ class SequenceController < ApplicationController
   def input; end
 
   def view
+  begin
     @input = validate_input params[:values]
-
     subs_arr = find_increasing_subs.filter { |arr| arr.count > 1 }
     @subs = subs_arr.map { |subseq| subseq.join(' ') }
     @result = subs_arr.max_by(&:length).join(' ')
-
     @input = @input.join(' ')
+
   rescue StandardError => e
     @error = case e.class.to_s
              when 'ArgumentError' then 'Введены посторонние символы'
              when 'NoMethodError' then 'Последовательность не найдена'
              else e
              end
+  ensure
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: [{ name: :result, type: @result.class.to_s, value: @result }, 
+                      { name: :subs, type: @subs.class.to_s, value: @subs },
+                      { name: :error, type: @error.class.to_s, value: @error }]
+      end
+    end
+  end
   end
 
   private
@@ -34,7 +44,7 @@ class SequenceController < ApplicationController
     raise 'Вы ничего не ввели' if input.empty?
 
     result = input.split.map { |el| Integer el }
-    raise 'Последовательность короче 10 чисел' if input.length < 10
+    raise 'Последовательность короче 10 чисел' if result.length < 10
 
     result
   end
