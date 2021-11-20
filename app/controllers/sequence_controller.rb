07607ddf0baf0d7f66e7b61af5c0de1224ffc432
MrAlexol <Alex02072002@yandex.ru>
2021-11-20 22:46:43 +0300

    v. 2. Controller & view improvement

--- app/controllers/sequence_controller.rb
+++ app/controllers/sequence_controller.rb
@@ -5,24 +5,30 @@ class SequenceController < ApplicationController
   def input; end
 
   def view
-    @input = params[:values]&.split&.map { |el| Integer el }
-    raise StandardError, 'Seq length is less than 10' if @input.length < 10
+    raise 'Ошибочный GET-запрос' if params[:values].nil?
+    raise 'Вы ничего не ввели' if params[:values].empty?
 
-    @result = find_increasing_subseq&.map { |subseq| subseq.join(' ') }
-  rescue ArgumentError
-    @error = 'Некорректный ввод'
-  rescue NoMethodError
-    @error = 'Последовательность не найдена'
-  rescue StandardError
-    @error = 'Последовательность короче 10 чисел'
+    @input = params[:values].split.map { |el| Integer el }
+    raise 'Последовательность короче 10 чисел' if @input.length < 10
+
+    @result = @subs = find_increasing_subs.filter { |arr| arr.count > 1 }
+    @result = @result.max_by(&:length).join(' ')
+    @subs.map! { |subseq| subseq.join(' ') }
+    @input = @input.join(' ')
+  rescue StandardError => e
+    @error = case e.class.to_s
+             when 'ArgumentError' then 'Введены посторонние символы'
+             when 'NoMethodError' then 'Последовательность не найдена'
+             else e
+             end
   end
 
   private
 
-  def find_increasing_subseq
+  def find_increasing_subs
     @input.each_with_object([[]]) do |el, acc|
-      acc << [] if el <=> acc[-1][-1] && el <= acc[-1][-1]
+      acc << [] if acc[-1].any? && el <= acc[-1][-1]
       acc[-1] << el
-    end.filter { |arr| arr.count > 1 }.yield_self { |this| this.append(this.max_by(&:length)) }
+    end
   end
 end
